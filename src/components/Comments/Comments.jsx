44c7dd7fbaf96a73555da9b1a02e11407001693d
Bestnogm1 <koffiarielhessou@gmail.com>
2022-11-27 23:19:24 -0500

    quick patch fixed deplyed error

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -11,46 +11,48 @@ const Comments = ({ ticketDetailId }) => {
 
   return (
     <Chakra.Box className={styles.CommentsMain}>
-      {getAllMessage?.map((message, index) => (
-        <React.Fragment key={index}>
-          {message?.ticketId === ticketDetailId ? (
-            <Chakra.Box w="100%" className={styles.CommentsComp}>
-              <Chakra.Flex direction="row" gap="2rem">
-                <Chakra.Tooltip
-                  label={message.ownedBy.name}
-                  aria-label="A tooltip"
-                >
-                  <Chakra.Image
-                    borderRadius="full"
-                    boxSize="30px"
-                    src={message.ownedBy.profilePicture}
-                    alt={message.ownedBy.name}
-                  />
-                </Chakra.Tooltip>
-                <Chakra.Text>
-                  {dayjs().to(dayjs(message.createdAt))}
-                </Chakra.Text>
-              </Chakra.Flex>
-              <Chakra.Box className={styles.Comments}>
-                <Chakra.Flex>
-                  <Chakra.Text>{message?.content}</Chakra.Text>
-                </Chakra.Flex>
-              </Chakra.Box>
-              {message?.ownedBy.email === user.email ? (
-                <Chakra.Button
-                  size="small"
-                  color="red"
-                  variant="link"
-                  width="4rem"
-                  onClick={() => handleDeleteComments(message?.tempUUID)}
-                >
-                  Delete
-                </Chakra.Button>
+      {getAllMessage
+        ? getAllMessage?.map((message, index) => (
+            <React.Fragment key={index}>
+              {message?.ticketId === ticketDetailId ? (
+                <Chakra.Box w="100%" className={styles.CommentsComp}>
+                  <Chakra.Flex direction="row" gap="2rem">
+                    <Chakra.Tooltip
+                      label={message?.ownedBy.name}
+                      aria-label="A tooltip"
+                    >
+                      <Chakra.Image
+                        borderRadius="full"
+                        boxSize="30px"
+                        src={message.ownedBy.profilePicture}
+                        alt={message.ownedBy.name}
+                      />
+                    </Chakra.Tooltip>
+                    <Chakra.Text>
+                      {dayjs().to(dayjs(message.createdAt))}
+                    </Chakra.Text>
+                  </Chakra.Flex>
+                  <Chakra.Box className={styles.Comments}>
+                    <Chakra.Flex>
+                      <Chakra.Text>{message?.content}</Chakra.Text>
+                    </Chakra.Flex>
+                  </Chakra.Box>
+                  {message?.ownedBy.email === user.email ? (
+                    <Chakra.Button
+                      size="small"
+                      color="red"
+                      variant="link"
+                      width="4rem"
+                      onClick={() => handleDeleteComments(message?.tempUUID)}
+                    >
+                      Delete
+                    </Chakra.Button>
+                  ) : null}
+                </Chakra.Box>
               ) : null}
-            </Chakra.Box>
-          ) : null}
-        </React.Fragment>
-      ))}
+            </React.Fragment>
+          ))
+        : null}
     </Chakra.Box>
   );
 };
